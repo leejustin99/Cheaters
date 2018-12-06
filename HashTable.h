f09b4c1078d99611a0036a1fca1f26df6096c411
MyNameIsHubert <36801976+MyNameIsHubert@users.noreply.github.com>
2018-12-06 17:00:05 -0600

    Files

---
+++ HashTable.h
@@ -0,0 +1,71 @@
+//
+// Created by Hubert ECE on 12/6/2018.
+//
+
+#ifndef PLAGIARISMCATCHER_HASHTABLE_H
+#define PLAGIARISMCATCHER_HASHTABLE_H
+
+#endif //PLAGIARISMCATCHER_HASHTABLE_H
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
+#define TABLE_SIZE 1024
+using namespace std;
+class HashTable {
+public:
+    HashTable();
+
+    HashTable(const HashTable &source);
+
+    ~HashTable();
+
+    void insertItem(string &value, int key)
+    {
+        struct Node *node1;
+        node1->value = value;
+        node1->key = key;
+        node1->next = NULL;
+
+        if(Table[key].key == 0){
+            Table[key] = *node1;
+        }
+        else
+        {
+            Table[key].next = node1;
+        }
+
+    }
+
+    bool CollisionFinder(int key, string &name , int numbertocheck)
+    {
+
+            for(int i=0;i<TABLE_SIZE;i++){
+                if(Table[i].next != NULL){
+                    if(Table[i].next->value != Table[i].value)
+                    {
+                        return true;
+                    }
+                }
+
+            }
+    }
+
+
+private:
+    struct Node {
+        int key;
+        string value;
+        Node *next;
+    };
+
+    struct Node Table[TABLE_SIZE];
+
+};
--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@
 #include <vector>
 #include <string>
 #include <iostream>
-
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <cstring>
 using namespace std;
-
+void print(std::vector<string> const &input);
 /*function... might want it in some class?*/
 int getdir (string dir, vector<string> &files)
 {
@@ -24,7 +28,7 @@ int getdir (string dir, vector<string> &files)
     return 0;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     string dir = string("sm_doc_set");
     vector<string> files = vector<string>();
@@ -34,5 +38,35 @@ int main()
     for (unsigned int i = 0;i < files.size();i++) {
         cout << i << files[i] << endl;
     }
+
+    string fl=dir+ "/"+files[3].c_str();
+    ifstream file;
+    file.open(fl.c_str());
+
+    cout<<"a" << endl;
+
+    vector<string> chunks;
+    chunks.clear();
+    while(!file.eof())
+    {
+        string s;
+        while(chunks.size() < atoi(argv[2]))
+        {
+            file>>s;
+            chunks.push_back(s);
+        }
+        print(chunks);
+        chunks.erase(chunks.begin());
+        cout<<endl;
+        cout<<endl;
+    }
     return 0;
+}
+
+void print(vector<string> const &input)
+{
+    for(int  i =0; i<input.size(); i++)
+    {
+        cout << input[i] << endl;
+    }
 }
